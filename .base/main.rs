21a4ec220da920e44e5c06a17235a3a8299a3f25
Christian Grasso <chris54721@outlook.com>
2019-12-01 10:59:49 +0100

    Add day creation script

---
+++ .base/main.rs
@@ -0,0 +1,5 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("./%DIR%/input/input.txt").unwrap();
+}
---
+++ makemyday.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -e
+
+[ -z "$1" ] && echo "Usage: ./makemyday.sh [day]"
+
+DIR="day$1"
+sed -i '$ d' Cargo.toml
+printf '    \"%s\",\n]' "$DIR" >> Cargo.toml
+cargo new "$DIR"
+sed "s/%DIR%/$DIR/g" .base/main.rs > "$DIR/src/main.rs"
