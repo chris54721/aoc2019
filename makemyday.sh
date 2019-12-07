61b04ea1efc17cf678539910a6145380c592a2d0
Christian Grasso <chris54721@outlook.com>
2019-12-07 18:32:08 +0100

    Update day setup script

--- makemyday.sh
+++ makemyday.sh
@@ -8,3 +8,5 @@ sed -i '$ d' Cargo.toml
 printf '    \"%s\",\n]' "$DIR" >> Cargo.toml
 cargo new "$DIR"
 sed "s/%DIR%/$DIR/g" .base/main.rs > "$DIR/src/main.rs"
+mkdir "$DIR/input"
+touch "$DIR/input/input.txt"
